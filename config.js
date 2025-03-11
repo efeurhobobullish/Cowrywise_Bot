9c05143959a9d976e678cc8a978995911692a5f9
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 11:24:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     TELEGRAM_BOT_TOKEN: "7637161395:AAF6T9HsFUi3qCFMmC2ZxR1Me0oITZrLsOY",
     PORT: 3000,
+    RENDER_URL: "https://cowrywise-bot.onrender.com",
     OWNER_NAME: "𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
     OWNER_NUMBER: "+2348144250768",
     OWNER_USERNAME: "only_one_empire",
