1c5013542c1b8c4d4bff3ab7b99aefc3649b4e49
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 10:55:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    TELEGRAM_BOT_TOKEN: "your-telegram-bot-token",
+    TELEGRAM_BOT_TOKEN: "7637161395:AAF6T9HsFUi3qCFMmC2ZxR1Me0oITZrLsOY",
     PORT: 3000,
     OWNER_NAME: "𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
     OWNER_NUMBER: "+2348144250768",
     OWNER_USERNAME: "only_one_empire",
     OWNER_CAPTION: "© 2025–2026 𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
-    ADMIN_ID: "6307156448",
+    ADMIN_ID: "7128227681",
     CHANNELS: ["@cowrywise_gaming_channel"]
 };
