6ead009c947f5dd99f13ce5230ac19cdb4e7d08e
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 10:34:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const TelegramBot = require("node-telegram-bot-api");
+const config = require("./config");
+
+const bot = new TelegramBot(config.BOT_TOKEN, { polling: true });
+
+// Load all command plugins
+const fs = require("fs");
+const path = require("path");
+
+const pluginsPath = path.join(__dirname, "plugins");
+fs.readdirSync(pluginsPath).forEach(file => {
+  if (file.endsWith(".js")) {
+    const command = require(`./plugins/${file}`);
+    command(bot);
+  }
+});
+
+console.log("🤖 Bot is running...");
