26745506228a00b285b9026e710feb7ad2849613
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 10:44:21 +0100

    𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
     OWNER_NAME: "𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
     OWNER_NUMBER: "+2348144250768",
     OWNER_USERNAME: "only_one_empire",
-    OWNER_CAPTION: "© 2025–2026 𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁"
+    OWNER_CAPTION: "© 2025–2026 𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
+    ADMIN_ID: "6307156448",
+    CHANNELS: ["@cowrywise_gaming_channel"]
 };
