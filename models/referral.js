2f1505e1074493cff88d7c0a9a43fa63b996c74a
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 20:08:11 +0100

    Update referral.js

--- models/referral.js
+++ models/referral.js
@@ -4,7 +4,7 @@ const referralSchema = new mongoose.Schema({
     userId: { type: Number, required: true, unique: true },
     referralLink: { type: String, required: true },
     referrals: { type: Number, default: 0 },
-    balance: { type: mongoose.Decimal128, default: 0.00 }
+    balance: { type: Number, default: 0.00 }
 });
 
-module.exports = mongoose.model("referral", referralSchema);
+module.exports = mongoose.model("Referral", referralSchema);
