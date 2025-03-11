8a82dd64c069fefea17515d7300642b1ce3c52dd
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-11 10:33:22 +0100

    © 2025–2026 𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TELEGRAM_BOT_TOKEN: "your-telegram-bot-token",
+    PORT: 3000,
+    OWNER_NAME: "𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁",
+    OWNER_NUMBER: "+2348144250768",
+    OWNER_USERNAME: "only_one_empire",
+    OWNER_CAPTION: "© 2025–2026 𝖤𝗆𝗉𝗂𝗋𝖾 𝖳𝖾𝖼𝗁"
+};
